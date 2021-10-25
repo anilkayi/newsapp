3e392110cf40a7a6badcd7c72252a84aef009b01
DESKTOP-P9DVLCP\user <muratanil70@gmail.com>
2021-10-26 02:51:24 +0300

    api and detail page added

--- lib/NewsPage.dart
+++ lib/NewsPage.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:newsapp/NewsDetail.dart';
+import 'package:newsapp/data/newsdata.dart';
+import 'package:newsapp/models/newsmodels.dart';
 
 class NewsPage extends StatefulWidget {
   const NewsPage({Key? key}) : super(key: key);
@@ -8,13 +12,76 @@ class NewsPage extends StatefulWidget {
 }
 
 class _NewsPageState extends State<NewsPage> {
-  TextEditingController searchController = TextEditingController();
+  NewsData getData = NewsData();
+  NewsModels getApi = NewsModels();
+
+  Future<void> getTrNews() async {
+    getApi = await getData.getNews();
+  }
+
+  @override
+  void initState() {
+    getTrNews();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.black,
+        centerTitle: true,
         title: Text('Haberler Uygulamasi'),
+      ),
+      body: FutureBuilder(
+        future: getTrNews(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return ListView.builder(
+                itemCount: getApi.articles!.length,
+                itemBuilder: (context, index) {
+                  return SafeArea(
+                    child: Container(
+                      height: 500,
+                      width: 500,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage('assets/images/a.jpg'),
+                            fit: BoxFit.cover),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Image.network(
+                              "${getApi.articles![index].urlToImage}"),
+                          ExpansionTile(
+                            title: Text('${getApi.articles![index].title}'),
+                            children: [
+                              ListTile(
+                                title: Text(
+                                    '${getApi.articles![index].description}'),
+                              ),
+                            ],
+                          ),
+                          TextButton(
+                              onPressed: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => NewsDetail(getApi
+                                            .articles![index].url
+                                            .toString())));
+                              },
+                              child: Text('İşte Detaylar',
+                                  style: TextStyle(color: Colors.black)))
+                        ],
+                      ),
+                    ),
+                  );
+                });
+          }
+          return CircularProgressIndicator();
+        },
       ),
     );
   }
---
+++ lib/models/newsmodels.dart
@@ -0,0 +1,107 @@
+// To parse this JSON data, do
+//
+//     final newsModels = newsModelsFromMap(jsonString);
+
+import 'dart:convert';
+
+class NewsModels {
+  NewsModels({
+    this.status,
+    this.totalResults,
+    this.articles,
+  });
+
+  String? status;
+  int? totalResults;
+  List<Article>? articles;
+
+  factory NewsModels.fromJson(String str) =>
+      NewsModels.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory NewsModels.fromMap(Map<String, dynamic> json) => NewsModels(
+        status: json["status"],
+        totalResults: json["totalResults"],
+        articles:
+            List<Article>.from(json["articles"].map((x) => Article.fromMap(x))),
+      );
+
+  Map<String, dynamic> toMap() => {
+        "status": status,
+        "totalResults": totalResults,
+        "articles": List<dynamic>.from(articles!.map((x) => x.toMap())),
+      };
+}
+
+class Article {
+  Article({
+    this.source,
+    this.author,
+    this.title,
+    this.description,
+    this.url,
+    this.urlToImage,
+    this.publishedAt,
+    this.content,
+  });
+
+  Source? source;
+  String? author;
+  String? title;
+  String? description;
+  String? url;
+  String? urlToImage;
+  DateTime? publishedAt;
+  String? content;
+
+  factory Article.fromJson(String str) => Article.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory Article.fromMap(Map<String, dynamic> json) => Article(
+        source: Source.fromMap(json["source"]),
+        author: json["author"] == null ? null : json["author"],
+        title: json["title"],
+        description: json["description"],
+        url: json["url"],
+        urlToImage: json["urlToImage"],
+        publishedAt: DateTime.parse(json["publishedAt"]),
+        content: json["content"] == null ? null : json["content"],
+      );
+
+  Map<String, dynamic> toMap() => {
+        "source": source!.toMap(),
+        "author": author == null ? null : author,
+        "title": title,
+        "description": description,
+        "url": url,
+        "urlToImage": urlToImage,
+        "publishedAt": publishedAt!.toIso8601String(),
+        "content": content == null ? null : content,
+      };
+}
+
+class Source {
+  Source({
+    this.id,
+    this.name,
+  });
+
+  dynamic? id;
+  String? name;
+
+  factory Source.fromJson(String str) => Source.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory Source.fromMap(Map<String, dynamic> json) => Source(
+        id: json["id"],
+        name: json["name"],
+      );
+
+  Map<String, dynamic> toMap() => {
+        "id": id,
+        "name": name,
+      };
+}
